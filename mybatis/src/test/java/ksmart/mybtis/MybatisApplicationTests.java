0c5b559c1f095476431ac7a9cb9f677041d0d09a
ECS <ECS@DESKTOP-048HU3O>
2024-10-11 15:36:24 +0900

    [ksmart] 초기 프로젝트 셋팅

---
+++ mybatis/src/test/java/ksmart/mybtis/MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package ksmart.mybtis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
